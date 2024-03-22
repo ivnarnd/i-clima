74f034461ed4b39127169ce05c85f364b3f93e9c
Ivan Aranda <iaranda264@gmail.com>
2024-03-22 12:55:14 -0300

    Agregando estilo incial de fuente

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import './WeatherApp.css';
 
 export const WeatherApp = () => {
+  const urlBase = 'https://api.openweathermap.org/data/2.5/weather';
+  const API_KEY = '52c2459ced2560b6e7eda9c2896de873';
+  const kelvDif = 273.15;
+
   return (
-    <div>Esta va a ser la app del clima</div>
+    <div>
+      <h1>Iclima</h1>
+      <form action="">
+          <input type="text" placeholder='Ingrese una Ciudad' />
+          <button type="submit">Buscar</button>
+      </form>
+    </div>
   )
 }
